patchew-project/patchew

commit f845f6359ae939d72c40db46710d676755944b0a

git: do not require superuser permissions to edit configuration
Currently, mods/git.py only shows the config box to superusers.  This
was a mistake, so fix it.

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mods/git.py
@@ -193,8 +193,6 @@ class GitModule(PatchewModule):
                                          })
 
     def prepare_project_hook(self, request, project):
-        if not request.user.is_superuser:
-            return
         project.extra_info.append({"title": "Git configuration",
                                    "class": "info",
                                    "content_html": self.build_config_html(request,

patchew-project/patchew-ci

commit 46cbc9f61f1b835db781aded5c8d745c654889ba

git: require maintainership to edit configuration
git info must not be visible without authentication.  Use maintained_by
as in other prepare_project_hook

Fixes: f845f6359ae939d72c40db46710d676755944b0a
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
Message-Id: <20180308075143.18626-1-pbonzini@redhat.com>

mods/git.py
@@ -193,6 +193,8 @@ class GitModule(PatchewModule):
                                          })
 
     def prepare_project_hook(self, request, project):
+        if not project.maintained_by(request.user):
+            return
         project.extra_info.append({"title": "Git configuration",
                                    "class": "info",
                                    "content_html": self.build_config_html(request,