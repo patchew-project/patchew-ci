68e5186bf4d995f79f62de6b32a32c59dac7f2ef
Paolo Bonzini <pbonzini@redhat.com>
2018-04-23 10:14:12 +0200

    add result renderer infrastructure
    Plugins can prepare the "status" section of the series/message detail
    pages from the same Result objects used by the REST API.  Record in the
    Result the object that created it, so that it can prepare an HTML
    rendering of the Result.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20180423081415.15099-2-pbonzini@redhat.com>

--- api/models.py
+++ api/models.py
@@ -601,13 +601,18 @@ class Module(models.Model):
     def __str__(self):
         return self.name
 
-class Result(namedtuple("Result", "name status log log_url obj data")):
+class Result(namedtuple("Result", "name status log log_url obj data renderer")):
     __slots__ = ()
 
-    def __new__(cls, name, status, obj, log=None, log_url=None, data=None, request=None):
+    def __new__(cls, name, status, obj, log=None, log_url=None, data=None, request=None, renderer=None):
         if log_url is not None and request is not None:
             log_url = request.build_absolute_uri(log_url)
         if status not in ('pending', 'success', 'failure'):
             raise ValueError("invalid value '%s' for status field" % status)
         return super(cls, Result).__new__(cls, status=status, log=log, log_url=log_url,
-                                          obj=obj, data=data, name=name)
+                                          obj=obj, data=data, name=name, renderer=renderer)
+
+    def render(self):
+        if self.renderer is None:
+            return None
+        return self.renderer.render_result(self)
