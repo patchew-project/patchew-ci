e3c029dc4e0334913f4396dcbc20a7596120680f
Fam Zheng <famz@redhat.com>
2018-08-02 16:14:19 +0800

    manage.py: Run tests/ if no test specified
    Just to make ./manage.py slightly more usable.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Message-Id: <20180802081422.25982-2-famz@redhat.com>

--- manage.py
+++ manage.py
@@ -16,4 +16,7 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+    args = list(sys.argv)
+    if len(args) == 2 and args[1] == "test":
+        args.append("tests")
+    execute_from_command_line(args)
